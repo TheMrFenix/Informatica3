af31120996c8d00a83c1bea9863631f8af2375a0
stef <stefhurtadomurn1@gmail.com>
2023-11-14 11:44:32 -0400

    conect

---
+++ src/Parcial3/proyecto/conexion java-mysql/Conexion.java
@@ -0,0 +1,30 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    String url="jdbc:mysql://localhost:3306/";
+    String db="dispositivosmoviles";
+    String user="root";
+    String password="";
+    String driver="com.mysql.cj.jdbc.Driver";
+    Connection jdbc;
+    public Conexion(){}
+    public Connection conectar(){
+        try{
+            Class.forName(driver);
+            jdbc=DriverManager.getConnection(url+db,user,password);
+        }catch (ClassNotFoundException | SQLException e){
+            System.out.println("No se conecto a la base de datos "+ e);
+        }
+        return jdbc;
+    }
+    public void desconectar(){
+        try {
+            jdbc.close();
+        }catch (SQLException e){
+            System.out.println("Error al finaizar la conexion " + e);
+        }
+    }
+}
+
